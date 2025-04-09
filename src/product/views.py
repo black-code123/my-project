516023b3172eda111d85264a1de6d6410295922c
black-code123 <b81989518@gmail.com>
2025-04-09 16:54:48 +0200

    add all project

---
+++ src/product/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Product
+from django.views.generic import ListView , DetailView
+# Create your views here.
+
+class ProductList(ListView):
+    model = Product
+
+class ProductDetail(DetailView):
+    model = Product
